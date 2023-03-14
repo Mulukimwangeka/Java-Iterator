98e15aab3a6bf7cb310cf42b120352cfab950056
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-14 09:37:05 +0300

    iterator extra example added

--- src/Main.java
+++ src/Main.java
@@ -18,3 +18,21 @@ public class Main {
         System.out.println(it.next());
     }
 }
+
+//public class Main {
+//    public static void main(String[] args) {
+//        ArrayList<Integer> numbers = new ArrayList<Integer>();
+//        numbers.add(12);
+//        numbers.add(8);
+//        numbers.add(2);
+//        numbers.add(23);
+//        Iterator<Integer> it = numbers.iterator();
+//        while(it.hasNext()) {
+//            Integer i = it.next();
+//            if(i < 10) {
+//                it.remove();
+//            }
+//        }
+//        System.out.println(numbers);
+//    }
+//}
